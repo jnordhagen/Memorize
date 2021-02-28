7ed7c052ead0deedd1bad5f30938c81475625858
Jakob Nordhagen <jakobn@stanford.edu>
2021-02-28 12:54:11 -0800

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Jakob Nordhagen on 2/28/21.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
